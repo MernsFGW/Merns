e62330d43b00dcabe01a2ed86f878ba61ea63c36
Vo Minh Quoc (Sagit) <84282841+Sagit612@users.noreply.github.com>
2023-02-09 23:37:49 +0700

    Fix bug for nodemon and mongodb

--- config/config.js
+++ config/config.js
@@ -2,9 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_SECRET_KEY",
-    mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST ||
-                'mongodb://' + (process.env.IP || 'localhost') + ':' +
-                (process.env.MONGO_PORT || '27017') + '/MernsLab'
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0.edsdngb.mongodb.net/?retryWrites=true&w=majority",
 }
 
 export default config
