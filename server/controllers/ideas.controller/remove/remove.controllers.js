5ff1073b245cc602fff8a79d7c2985fbc633363d
Vo Minh Quoc (Sagit) <84282841+Sagit612@users.noreply.github.com>
2023-02-24 12:19:08 +0700

    fix function update idea and delete idea

--- server/controllers/ideas.controller/remove/remove.controllers.js
+++ server/controllers/ideas.controller/remove/remove.controllers.js
@@ -1,11 +1,26 @@
 import errorHandler from '../../../helpers/dbErrorHandler.js';
+import cloudinary from '../../../helpers/cloudinary.js';
 
 const remove = async (req, res) => {
     try {
         let idea = req.information;
+        let public_id = idea.photo.public_id;
+        let url = idea.photo.url;
+        let image
+        const photoResult = await cloudinary.uploader
+            .destroy(public_id, (err,result) =>
+                    {
+                        if (err) {
+                            throw errorHandler.getErrorMessage(err)
+                        } else {
+                            image = result;
+                        }
+                    }
+        );
         let deletedIdea = await idea.remove();
         res.status(200).json({
             idea: deletedIdea,
+            image: image,
             message: "Deleted idea Successfully"
         });
     } catch (err) {
