988ca7c68cc0002588786409c9fc82050beffed0
Trịnh Đỗ Duy Hưng <40053464+trinhdoduyhungss@users.noreply.github.com>
2023-02-28 10:35:44 +0700

    Delete search.routes.js

--- server/routes/user.routes/search.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import verifyToken from '../../helpers/verifyToken'
-import userCtrl from '../../controllers/users.controller/id/id.controller'
-
-const router = express.Router();
-
-router.route('/api/users/:id').get(verifyToken,userCtrl.userById);
-
-export default router;
