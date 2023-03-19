0770e055e2fd59f4be7c0c7dbcf7d7126975f2a2
Trịnh Đỗ Duy Hưng <40053464+trinhdoduyhungss@users.noreply.github.com>
2023-03-19 14:34:18 +0700

    Fix load more api feedback

--- server/controllers/feedbacks.controller/list/list.controllers.js
+++ server/controllers/feedbacks.controller/list/list.controllers.js
@@ -7,9 +7,12 @@ const listFeedbacks = async (req, res) => {
     const startIndex = parseInt(start) || 0;
     const endIndex = parseInt(end) || 10;
     const feedbacks = await Feedback.find(ideaId?{ideaId}:{})
-      .sort({ createdAt: -1 })
       .skip(startIndex)
       .limit(endIndex - startIndex);
+    // sort by created date
+    feedbacks.sort((a, b) => {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    });
     res.json(feedbacks);
   } catch (err) {
     return res.status(400).json({
